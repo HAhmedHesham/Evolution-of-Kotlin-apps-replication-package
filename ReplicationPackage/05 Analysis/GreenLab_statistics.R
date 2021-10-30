9f1ce05f04f81ec1a2579b50976263ad44c87651
Kalle Janssen <Kallejanssen@me.com>
2021-10-30 11:55:32 +0200

    cleaned code a bit

--- ReplicationPackage/05 Analysis/GreenLab_statistics.R
+++ ReplicationPackage/05 Analysis/GreenLab_statistics.R
@@ -37,56 +37,7 @@ p <- ggplot(data[data$browser=="chrome", ], aes(x = crit, y=fcp, group=crit)) +
   geom_boxplot() + xlab("Critical CSS") + ylab("First contentful paint (ms)")
 p + stat_summary(fun=mean, geom="point", shape=23, size=4) + theme(text = element_text(size = 20)) + scale_x_discrete(labels=c("Not applied", "Applied"))
 
-# check normality
-check_normality <- function(dataset_to_eval, x, title) {
-  plot(density(dataset_to_eval), xlab=x, main=paste("Density plot for", title, sep=" "))
-  qqnorm(dataset_to_eval, main=paste("Normal Q-Q Plot for", title, sep=" "))
-  print(shapiro.test(dataset_to_eval))
-}
-
-# Chrome, critical = 0
-check_normality(data[which(data$browser == "chrome" & data$crit==0), ]$lt, "Loading time (ms)", "chrome, crit = 0, loading time")
-check_normality(data[which(data$browser == "chrome" & data$crit==0), ]$fp, "First paint (ms)", "chrome, crit = 0, first paint")
-check_normality(data[which(data$browser == "chrome" & data$crit==0), ]$fcp, "First contentful paint (ms)", "chrome, crit = 0, first contentful paint")
-check_normality(data[which(data$browser == "chrome" & data$crit==0), ]$e, "Energy usage (J)", "chrome, crit = 0, energy usage")
-
-# Chrome, critical = 1
-check_normality(data[which(data$browser == "chrome" & data$crit==1), ]$lt, "Loading time (ms)", "chrome, crit = 1, loading time")
-check_normality(data[which(data$browser == "chrome" & data$crit==1), ]$fp, "First paint (ms)", "chrome, crit = 1, first paint")
-check_normality(data[which(data$browser == "chrome" & data$crit==1), ]$fcp, "First contentful paint (ms)", "chrome, crit = 1, first contentful paint")
-check_normality(data[which(data$browser == "chrome" & data$crit==1), ]$e, "Energy usage (J)", "chrome, crit = 1, energy usage")
-
-# Firefox, critical = 0
-check_normality(data[which(data$browser == "firefox" & data$crit==0), ]$lt, "Loading time (ms)", "firefox, crit = 0, loading time")
-check_normality(data[which(data$browser == "firefox" & data$crit==0), ]$fp, "First paint (ms)", "firefox, crit = 0, first paint")
-check_normality(data[which(data$browser == "firefox" & data$crit==0), ]$fcp, "First contentful paint (ms)", "firefox, crit = 0, first contentful paint")
-check_normality(data[which(data$browser == "firefox" & data$crit==0), ]$e, "Energy usage (J)", "firefox, crit = 0, energy usage")
-
-# Firefox, critical = 1
-check_normality(data[which(data$browser == "firefox" & data$crit==1), ]$lt, "Loading time (ms)", "firefox, crit = 1, loading time")
-check_normality(data[which(data$browser == "firefox" & data$crit==1), ]$fp, "First paint (ms)", "firefox, crit = 1, first paint")
-check_normality(data[which(data$browser == "firefox" & data$crit==1), ]$fcp, "First contentful paint (ms)", "firefox, crit = 1, first contentful paint")
-check_normality(data[which(data$browser == "firefox" & data$crit==1), ]$e, "Energy usage (J)", "firefox, crit = 1, energy usage")
-
-# Figure Q-Q plots 
-par(mfrow=c(2,2))
-qqnorm(data[which(data$browser == "chrome" & data$crit==0), ]$fcp, main="Density plot for chrome, crit = 0, first contentful paint")
-qqnorm(data[which(data$browser == "chrome" & data$crit==1), ]$fcp, main="Density plot for chrome, crit = 1, first contentful paint")
-qqnorm(data[which(data$browser == "chrome" & data$crit==0), ]$e, main="Density plot for chrome, crit = 0, energy usage")
-qqnorm(data[which(data$browser == "chrome" & data$crit==1), ]$e, main="Density plot for chrome, crit = 1, energy usage")
-
-par(mfrow=c(1,1))
-plot(density(data[which(data$browser == "chrome" & data$crit==0), ]$fcp), 
-     xlab="First contentful paint (ms)", ylim=c(0,0.0012), col="blue", 
-     main="With and without Critical CSS of first contentful paint in Chrome",
-     cex.main=2, cex.lab=1.5)
-lines(density(data[which(data$browser == "chrome" & data$crit==1), ]$fcp), col="red")
-legend("topright",
-       c("Critical CSS applied", "Critical CSS not applied"),
-       fill=c("red","blue"),
-       cex=1.5
-)
-
+# Density plots
 #Chrome
 par(mfrow=c(2,2))
 plot(density(data[which(data$browser == "chrome" & data$crit==0), ]$lt), 
@@ -179,37 +130,62 @@ legend("topright",
        cex=1.5
 )
 
-data1 = data[which(data$browser == "chrome" & data$crit==0), ]$lt
-data2 = data[which(data$browser == "chrome" & data$crit==1), ]$lt
-wilcox.test(data1, data2, paired=TRUE)
-cliff.delta(data2, data1)
-data1 = data[which(data$browser == "chrome" & data$crit==0), ]$fp
-data2 = data[which(data$browser == "chrome" & data$crit==1), ]$fp
-wilcox.test(data1, data2, paired=TRUE)
-cliff.delta(data2, data1)
-data1 = data[which(data$browser == "chrome" & data$crit==0), ]$fcp
-data2 = data[which(data$browser == "chrome" & data$crit==1), ]$fcp
-wilcox.test(data1, data2, paired=TRUE)
-cliff.delta(data2, data1)
-data1 = data[which(data$browser == "chrome" & data$crit==0), ]$e
-data2 = data[which(data$browser == "chrome" & data$crit==1), ]$e
-wilcox.test(data1, data2, paired=TRUE)
-cliff.delta(data2, data1)
+# Check normality
+check_normality <- function(dataset_to_eval, x, title) {
+  plot(density(dataset_to_eval), xlab=x, main=paste("Density plot for", title, sep=" "))
+  qqnorm(dataset_to_eval, main=paste("Normal Q-Q Plot for", title, sep=" "))
+  print(shapiro.test(dataset_to_eval))
+}
 
-data1 = data[which(data$browser == "firefox" & data$crit==0), ]$lt
-data2 = data[which(data$browser == "firefox" & data$crit==1), ]$lt
-wilcox.test(data1, data2, paired=TRUE)
-cliff.delta(data2, data1)
-data1 = data[which(data$browser == "firefox" & data$crit==0), ]$fp
-data2 = data[which(data$browser == "firefox" & data$crit==1), ]$fp
-wilcox.test(data1, data2, paired=TRUE)
-cliff.delta(data2, data1)
-data1 = data[which(data$browser == "firefox" & data$crit==0), ]$fcp
-data2 = data[which(data$browser == "firefox" & data$crit==1), ]$fcp
-wilcox.test(data1, data2, paired=TRUE)
-cliff.delta(data2, data1)
-data1 = data[which(data$browser == "firefox" & data$crit==0), ]$e
-data2 = data[which(data$browser == "firefox" & data$crit==1), ]$e
-wilcox.test(data1, data2, paired=TRUE)
-cliff.delta(data2, data1)
+# Chrome, critical = 0
+check_normality(data[which(data$browser == "chrome" & data$crit==0), ]$lt, "Loading time (ms)", "chrome, crit = 0, loading time")
+check_normality(data[which(data$browser == "chrome" & data$crit==0), ]$fp, "First paint (ms)", "chrome, crit = 0, first paint")
+check_normality(data[which(data$browser == "chrome" & data$crit==0), ]$fcp, "First contentful paint (ms)", "chrome, crit = 0, first contentful paint")
+check_normality(data[which(data$browser == "chrome" & data$crit==0), ]$e, "Energy usage (J)", "chrome, crit = 0, energy usage")
 
+# Chrome, critical = 1
+check_normality(data[which(data$browser == "chrome" & data$crit==1), ]$lt, "Loading time (ms)", "chrome, crit = 1, loading time")
+check_normality(data[which(data$browser == "chrome" & data$crit==1), ]$fp, "First paint (ms)", "chrome, crit = 1, first paint")
+check_normality(data[which(data$browser == "chrome" & data$crit==1), ]$fcp, "First contentful paint (ms)", "chrome, crit = 1, first contentful paint")
+check_normality(data[which(data$browser == "chrome" & data$crit==1), ]$e, "Energy usage (J)", "chrome, crit = 1, energy usage")
+
+# Firefox, critical = 0
+check_normality(data[which(data$browser == "firefox" & data$crit==0), ]$lt, "Loading time (ms)", "firefox, crit = 0, loading time")
+check_normality(data[which(data$browser == "firefox" & data$crit==0), ]$fp, "First paint (ms)", "firefox, crit = 0, first paint")
+check_normality(data[which(data$browser == "firefox" & data$crit==0), ]$fcp, "First contentful paint (ms)", "firefox, crit = 0, first contentful paint")
+check_normality(data[which(data$browser == "firefox" & data$crit==0), ]$e, "Energy usage (J)", "firefox, crit = 0, energy usage")
+
+# Firefox, critical = 1
+check_normality(data[which(data$browser == "firefox" & data$crit==1), ]$lt, "Loading time (ms)", "firefox, crit = 1, loading time")
+check_normality(data[which(data$browser == "firefox" & data$crit==1), ]$fp, "First paint (ms)", "firefox, crit = 1, first paint")
+check_normality(data[which(data$browser == "firefox" & data$crit==1), ]$fcp, "First contentful paint (ms)", "firefox, crit = 1, first contentful paint")
+check_normality(data[which(data$browser == "firefox" & data$crit==1), ]$e, "Energy usage (J)", "firefox, crit = 1, energy usage")
+
+# Figure Q-Q plots 
+par(mfrow=c(2,2))
+qqnorm(data[which(data$browser == "chrome" & data$crit==0), ]$fcp, main="Density plot for chrome, crit = 0, first contentful paint")
+qqnorm(data[which(data$browser == "chrome" & data$crit==1), ]$fcp, main="Density plot for chrome, crit = 1, first contentful paint")
+qqnorm(data[which(data$browser == "chrome" & data$crit==0), ]$e, main="Density plot for chrome, crit = 0, energy usage")
+qqnorm(data[which(data$browser == "chrome" & data$crit==1), ]$e, main="Density plot for chrome, crit = 1, energy usage")
+
+# Wilcoxon signed rank tests
+wilcox.test(data[which(data$browser == "chrome" & data$crit==0), ]$lt, data[which(data$browser == "chrome" & data$crit==1), ]$lt, paired=TRUE)
+wilcox.test(data[which(data$browser == "chrome" & data$crit==0), ]$fp, data[which(data$browser == "chrome" & data$crit==1), ]$fp, paired=TRUE)
+wilcox.test(data[which(data$browser == "chrome" & data$crit==0), ]$fcp, data[which(data$browser == "chrome" & data$crit==1), ]$fcp, paired=TRUE)
+wilcox.test(data[which(data$browser == "chrome" & data$crit==0), ]$e, data[which(data$browser == "chrome" & data$crit==1), ]$e, paired=TRUE)
+
+wilcox.test(data[which(data$browser == "firefox" & data$crit==0), ]$lt, data[which(data$browser == "firefox" & data$crit==1), ]$lt, paired=TRUE)
+wilcox.test(data[which(data$browser == "firefox" & data$crit==0), ]$fp, data[which(data$browser == "firefox" & data$crit==1), ]$fp, paired=TRUE)
+wilcox.test(data[which(data$browser == "firefox" & data$crit==0), ]$fcp, data[which(data$browser == "firefox" & data$crit==1), ]$fcp, paired=TRUE)
+wilcox.test(data[which(data$browser == "firefox" & data$crit==0), ]$e, data[which(data$browser == "firefox" & data$crit==1), ]$e, paired=TRUE)
+
+# Cliff delta
+cliff.delta(data[which(data$browser == "chrome" & data$crit==1), ]$lt, data[which(data$browser == "chrome" & data$crit==0), ]$lt)
+cliff.delta(data[which(data$browser == "chrome" & data$crit==1), ]$fp, data[which(data$browser == "chrome" & data$crit==0), ]$fp)
+cliff.delta(data[which(data$browser == "chrome" & data$crit==1), ]$fcp, data[which(data$browser == "chrome" & data$crit==0), ]$fcp)
+cliff.delta(data[which(data$browser == "chrome" & data$crit==1), ]$e, data[which(data$browser == "chrome" & data$crit==0), ]$e)
+
+cliff.delta(data[which(data$browser == "firefox" & data$crit==1), ]$lt, data[which(data$browser == "firefox" & data$crit==0), ]$lt)
+cliff.delta(data[which(data$browser == "firefox" & data$crit==1), ]$fp, data[which(data$browser == "firefox" & data$crit==0), ]$fp)
+cliff.delta(data[which(data$browser == "firefox" & data$crit==1), ]$fcp, data[which(data$browser == "firefox" & data$crit==0), ]$fcp)
+cliff.delta(data[which(data$browser == "firefox" & data$crit==1), ]$e, data[which(data$browser == "firefox" & data$crit==0), ]$e)
